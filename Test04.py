5f1b160c8ffd9eda98976b479cb4188dc231b797
504697121@qq.com <lpyinbeijing123>
2018-01-17 16:28:03 +0800

    增加doubanTop250爬虫

---
+++ Test04.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import codecs
+
+BASE_URL = 'http://movie.douban.com/top250'
+
+
+def load_page(url):
+    data = requests.get(url).content
+    return data
+
+
+def format_html(html):
+    soup = BeautifulSoup(html)
+    var = soup.find('ol', {'class': 'grid_view'})
+    movie_list = []
+    for movie in var.findAll('li'):
+        title = movie.find('div', {'class': 'hd'}).find('span', {'class': 'title'}).getText()
+        movie_list.append(title)
+
+    if get_next(html):
+        return movie_list, BASE_URL + get_next(html)
+    return movie_list, None
+
+
+def get_next(html):
+    soup = BeautifulSoup(html)
+    try:
+        return soup.find('span', {'class': 'next'}).find('a')['href']
+    except Exception as e:
+        return None
+
+
+def main():
+    next_url = BASE_URL
+    with codecs.open('movies', 'wb', encoding='utf-8')as fp:
+        while next_url:
+            movies, next_url = format_html(load_page(next_url))
+            fp.write(u'{movies}\n'.format(movies='\n'.join(movies)))
+            print(movies)
+
+
+if __name__ == '__main__':
+    main()
