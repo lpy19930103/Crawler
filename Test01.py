e8631ff69a159ab2fe7fef5548f31988f02a054f
504697121@qq.com <lpyinbeijing123>
2018-01-17 10:27:48 +0800

    Initial commit

---
+++ Test01.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+
+if title:
+    print(title)
+else:
+    print('title is none')
