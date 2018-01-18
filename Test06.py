b1c04a3e6d2e017f6c2628c7e19fc34d8d450873
504697121@qq.com <lpyinbeijing123>
2018-01-18 18:22:18 +0800

    爬取简书top30 输出csv

--- Test06.py
+++ Test06.py
@@ -4,6 +4,7 @@ import requests
 import json
 import codecs
 import time
+import csv
 
 BASE_URL = 'https://www.jianshu.com'
 TOP_30_URL = BASE_URL + '/trending/monthly?&page=%d'
@@ -25,7 +26,7 @@ def get_top_30(url):
     for note in note_list:
         content_link = note.find('div', {'class', 'content'}).find('a', {'class': 'title'})['href']
         content_title = note.find('div', {'class', 'content'}).find('a', {'class': 'title'}).getText()
-        list.append(json.dumps(Top30Item(content_title, BASE_URL + content_link).__dict__, ensure_ascii=False))
+        list.append(Top30Item(content_title, BASE_URL + content_link))
         print(json.dumps(Top30Item(content_title, BASE_URL + content_link).__dict__, ensure_ascii=False))
     return list
 
@@ -45,10 +46,19 @@ def main():
             print(e)
             break
 
-    with codecs.open("TOP30.txt", "wb", encoding='utf-8') as f:
-        for Top30Item in all:
-            f.write(u'{Top30Item}\n'.format(Top30Item='\n'.join(Top30Item)))
-            # f.write(Top30Item + '\n')
+    with codecs.open("TOP30.csv", "wb", encoding='utf-8') as f:
+        fieldnames = ['Title', 'Link']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in all:
+            for sigItem in item:
+                print(sigItem.title, sigItem.link)
+                writer.writerow({"Title": sigItem.title, "Link": sigItem.link})
+
+                # f.write(json.dumps(all, ensure_ascii=False))
+                # for Top30Item in all:
+                # f.write(u'{Top30Item}\n'.format(Top30Item='\n'.join(Top30Item)))
+                # f.write(Top30Item + '\n')
 
 
 if __name__ == '__main__':
