929bd838c748b9de68b6d5e7976b3df82e7fe9ff
504697121@qq.com <lpyinbeijing123>
2018-01-22 09:50:41 +0800

    查询 关键字 github 排行榜第一

--- Test07.py
+++ Test07.py
@@ -1,30 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-start_url = 'https://github.com/search?l=Java&o=desc&q=%s&s=stars&type=Repositories&utf8='
+start_url = 'https://github.com/search?l=Java&o=desc&p=%d&q=%s&s=stars&type=Repositories&utf8='
 
 base_url = 'https://github.com'
-key = "";
+key = ""
+page = 1
 
 
 def query():
-    html = requests.get(start_url % key).content
+    html = requests.get(start_url % (page, key)).content
     soup = BeautifulSoup(html)
     title = soup.find('title').getText()
-    info = soup.find('div', {'class': "col-8 pr-3"}).find('a')
-    link = base_url + info['href']
-    name = info.getText()
-    content = soup.find('p', {'class': 'col-9 d-inline-block text-gray mb-2 pr-4'}).getText()
     print(title)
-    print(name)
-    print(content)
-    print(link)
+
+    datas = soup.findAll('div', {'class': "repo-list-item d-flex flex-justify-start py-4 public source"})
+    for data in datas:
+        info = data.find('div', {'class': "col-8 pr-3"}).find('a')
+        link = base_url + info['href']
+        name = info.getText()
+        content = data.find('p', {'class': 'col-9 d-inline-block text-gray mb-2 pr-4'}).getText()
+        print(name.strip())
+        print(content.strip())
+        print(link)
+        print('-----------------------------------------------------------------------------------')
 
 
 def main():
+    global page
     global key
     key = input()
-    query()
+    while page < 6:
+        try:
+            page += 1
+            query()
+            time.sleep(5)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
